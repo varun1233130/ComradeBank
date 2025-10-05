ab98ec8beca5c1c7e414aa4fce0c6ac4ddf0000f
varun1233130 <149809355+varun1233130@users.noreply.github.com>
2025-10-05 12:22:39 +0530

    Initial commit: Added ComradeBankApp Spring Boot project

---
+++ src/main/java/ComradeBank/ComradeBankApplication.java
@@ -0,0 +1,13 @@
+package ComradeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComradeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComradeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ComradeBank/ComradeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ComradeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComradeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
